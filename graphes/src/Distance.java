3d97f6d44ebe04f51e91a7bd2fe0f346f702b6c3
Victor REMOND <victor96@hotmail.fr>
2017-05-19 15:36:56 +0200

    Vers l'infini et au-delà

--- graphes/src/Distance.java
+++ graphes/src/Distance.java
@@ -1,8 +1,106 @@
 
-public class Distance {
+public class Distance
+{
+	//Classe représentant la distance entre deux villes
+	protected Ville _ville1;
+	protected Ville _ville2;
+	//Distance à vol d'oiseau
+	protected double _distance_pigeon;
+	//Distance en suivant les routes
+	protected double _distance_coccinelle;
 
-	public Distance() {
-		// TODO Auto-generated constructor stub
+	public Distance()
+	{
+		this(null, null);
+	}
+
+	public Distance(Ville ville)
+	{
+		this(ville, null);
+	}
+
+	public Distance(Ville ville1, Ville ville2)
+	{
+		_ville1 = ville1;
+		_ville2 = ville2;
+		if((_ville1 != null) && (_ville2 != null))
+		{
+			calculDistances();
+		}
+	}
+
+	protected void calculDistances()
+	{
+		calculDistancePigeon();
+		calculDistanceCoccinelle();
+	}
+
+	protected void calculDistancePigeon()
+	{
+		if((_ville1 != null) && (_ville2 != null))
+		{
+			//calcul
+		}
+		else _distance_pigeon = 0;
+	}
+
+	protected void calculDistanceCoccinelle()
+	{
+		if((_ville1 != null) && (_ville2 != null))
+		{
+			//calcul
+		}
+		else _distance_coccinelle = 0;
+	}
+
+	public void setVille1(Ville ville)
+	{
+		_ville1 = ville;
+		calculDistances();
+	}
+
+	public void setVille2(Ville ville)
+	{
+		_ville2 = ville;
+		calculDistances();
+	}
+
+	public void setVilles(Ville ville1, Ville ville2)
+	{
+		_ville1 = ville1;
+		_ville2 = ville2;
+		calculDistances();
+	}
+
+	//Retourne la ville 1 pour l'index 1, la ville 2 pour l'index 2 et null pour tout autre index
+	public Ville getVille(int index)
+	{
+		if(index == 1)
+			return _ville1;
+		if(index == 2)
+			return _ville2;
+		return null;
+	}
+
+	//Retourne la ville à l'autre extrémité par rapport à celle passée en paramètre.
+	//Si la ville en paramètre représente les deux extrémités ou aucune des deux, retourne null
+	public Ville getVille(Ville depart)
+	{
+		if((_ville1 == depart) && (_ville2 != depart))
+			return _ville2;
+		if((_ville2 == depart) && (_ville1 != depart))
+			return _ville1;
+		return null;
+	}
+
+	public double distancePigeon()
+	{
+		return _distance_pigeon;
+	}
+
+	public double distanceCoccinelle()
+	{
+		return _distance_coccinelle;
 	}
 
 }
