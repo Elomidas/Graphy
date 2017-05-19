c970ec9ef932eb87cc5ab056e139f3c0a3693630
BastienJacoud <bastien.jacoud@etu.univ-lyon1.fr>
2017-05-19 15:32:28 +0200

    .

--- graphes/src/Ville.java
+++ graphes/src/Ville.java
@@ -17,4 +17,66 @@ public class Ville {
 		_dists = new ArrayList<Distance>();
 	}
 
+	public int getId()
+	{
+		return _id;
+	}
+
+	public void setId(int id)
+	{
+		if(id>= 0)
+			_id = id;
+	}
+
+	public String getNom()
+	{
+		return _nom;
+	}
+
+	public void setNom(String nom)
+	{
+		if(nom.compareTo("") != 0)
+			_nom = nom;
+	}
+
+	public int getNbHab()
+	{
+		return _nbHab;
+	}
+
+	public void setNbHab(int nbHab)
+	{
+		if(nbHab >= 0)
+			_nbHab = nbHab;
+	}
+
+	public Coordonnees getCoord()
+	{
+		return _coord;
+	}
+
+	public void setCoord(Coordonnees coord)
+	{
+		_coord.coordonneesOk(coord.longitude, coord.latitude);
+	}
+
+	public ArrayList<Distance> getDistances()
+	{
+		return _dists;
+	}
+
+	/*
+	public void setDictances(ArrayList<Distance> dists)
+	{
+		_dists = dists;
+	}
+	*/
+
+	public void ajouteDistance(Distance dist)
+	{
+		_dists.add(dist);
+	}
+
+	public void supprDistance()
+
 }
