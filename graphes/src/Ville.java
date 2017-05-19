6d818670c6cb08d99fb6e286df5232a56b52c6aa
BastienJacoud <bastien.jacoud@etu.univ-lyon1.fr>
2017-05-19 15:16:33 +0200

    .

--- graphes/src/Ville.java
+++ graphes/src/Ville.java
@@ -1,4 +1,20 @@
+import java.util.ArrayList;
 
 public class Ville {
+
+	private int _id;
+	private String _nom;
+	private int _nbHab;
+	private Coordonnees _coord;
+	private ArrayList<Distance> _dists;
+
+	public Ville()
+	{
+		_id = 0;
+		_nom = new String();
+		_nbHab = -1;
+		_coord = new Coordonnees();
+		_dists = new ArrayList<Distance>();
+	}
 
 }
